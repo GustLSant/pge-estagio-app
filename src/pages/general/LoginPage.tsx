bb0d5c5b07e72940395eadf522ba604ac9757e71
Gustavo LS <gustlsantdev@gmail.com>
2025-03-16 18:34:52 -0300

    creation loginPage

--- src/pages/general/LoginPage.tsx
+++ src/pages/general/LoginPage.tsx
@@ -1,10 +1,33 @@
+import TextInput from '../../components/TextInput/TextInput';
+import Button from '../../components/Button/Button'
+import { BiLogIn } from "react-icons/bi";
+import './LoginPage.css'
 
 
 export default function LoginPage(){
 
     return(
         <div className="login-page">
-            <h1>Login Page</h1>
+            <section className='login-page__left-section'>
+                <h1>Logo</h1>
+            </section>
+
+            <section className='login-page__right-section'>
+                <div className='right-section__login-container'>
+                    <div>
+                        <h1>Seja bem-vindo!</h1>
+                        <h2>Digite suas credenciais nos campos abaixo:</h2>
+                    </div>
+                    <div>
+                        <TextInput label='Email' fontSize='16px'  />
+                        <TextInput label='Senha' fontSize='16px' secret />
+                    </div>
+                    <div className='login-container__buttons-container'>
+                        <Button label='Entrar' variant='solid' rightIcon={BiLogIn} grow />
+                        <p className='login-container__forgot-password'>Esqueci minha senha</p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
