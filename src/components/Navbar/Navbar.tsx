ac5505aa6b8a648c53c753b438609508d4eeb637
Gustavo LS <gustlsantdev@gmail.com>
2025-03-18 02:18:20 -0300

    open and close animation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,9 +4,14 @@ import { useNavigate, useLocation } from 'react-router';
 import { BiFile } from "react-icons/bi";
 import { BiUserCircle } from "react-icons/bi";
 import { BiUser } from "react-icons/bi";
+import { BiArrowFromLeft } from "react-icons/bi";
 import './Navbar.css'
 
+type NavbarState = 'initialOpen' | 'open' | 'closed';
+
+
 export default function Navbar(){
+    const [state, setState] = useState<NavbarState>('initialOpen');
     const authContext = useContext(AuthContext);
     const currentUrl = useLocation().pathname;
     const navigate = useNavigate();
@@ -18,48 +23,62 @@ export default function Navbar(){
     }
 
 
+    function handleClick(){
+        if(state === 'closed'){ setState('open'); }
+    }
+
+    function handleMouseLeave(){
+        if(state === 'initialOpen' || state === 'open'){ setState('closed'); }
+    }
+
+
     return(
-        <div className="navbar">
-            <div className="navbar__header-container">
-                <img src="/logo-alt-transp-small.png" alt="Logo PGE-SE" />
-                <div>
-                    <h2>PGE - SE</h2>
-                    <small>
-                        Procuradoria Geral <br />
-                        do Estado de Sergipe
-                    </small>
-                </div>
-            </div>
+        <div className={`navbar ${(state === 'initialOpen' || state === 'open') ? 'active' : 'disabled'}`} onClick={handleClick} onMouseLeave={handleMouseLeave}>
+            <div className='navbar-wrapper'>
+                <section className='navbar__disabled-container'>
+                    <img className='navbar__disabled-logo' src="/logo-alt-transp-small.png" alt="Logo PGE-SE" />
+                    <BiArrowFromLeft />
+                </section>
 
-            <div className="navbar__options-container">
-                <div className="navbar__spacer"></div>
-                <div className={`navbar__option ${(currentUrl.includes('lista-processos'))?'active':''}`} onClick={()=>{handleOptionClick('process')}}>
-                    <BiFile />
-                    <p>Processos</p>
-                </div>
-                <div className="navbar__spacer"></div>
-                <div className={`navbar__option ${(currentUrl.includes('minha-conta'))?'active':''}`} onClick={()=>{handleOptionClick('conta')}}>
-                    <BiUserCircle />
-                    <p>Minha Conta</p>
-                </div>
-                <div className="navbar__spacer"></div>
-            </div>
+                <section className="navbar__header-container">
+                    <img src="/logo-alt-transp-small.png" alt="Logo PGE-SE" />
+                    <div>
+                        <h2>PGE - SE</h2>
+                        <small>
+                            Procuradoria Geral <br />
+                            do Estado de Sergipe
+                        </small>
+                    </div>
+                </section>
 
-            
-            <div className='navbar__foorter-container'>
+                <section className="navbar__options-container">
+                    <div className="navbar__spacer"></div>
+                    <div className={`navbar__option ${(currentUrl.includes('lista-processos'))?'active':''}`} onClick={()=>{handleOptionClick('process')}}>
+                        <BiFile />
+                        <p>Processos</p>
+                    </div>
+                    <div className="navbar__spacer"></div>
+                    <div className={`navbar__option ${(currentUrl.includes('minha-conta'))?'active':''}`} onClick={()=>{handleOptionClick('conta')}}>
+                        <BiUserCircle />
+                        <p>Minha Conta</p>
+                    </div>
+                    <div className="navbar__spacer"></div>
+                </section>
+                
                 <div className="navbar__spacer"></div>
-                <div className='navbar__foorter'>
+                
+                <section className='navbar__footer'>
                     <BiUser />
                     <div>
                         <p>{authContext?.user?.shortName}</p>
                         <small>
                             {
-                                (authContext?.user?.role === 'client') ? 'Cliente' : 
+                                (authContext?.user?.role === 'client') ? 'Cliente' :
                                 (authContext?.user?.role === 'attorney') ? 'Procurador' : ''
                             }
                         </small>
                     </div>
-                </div>
+                </section>
             </div>
         </div>
     )
