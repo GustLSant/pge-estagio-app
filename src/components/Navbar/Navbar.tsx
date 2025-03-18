6168347bb1cea234ef5eacb89863b37fc6eb7564
Gustavo LS <gustlsantdev@gmail.com>
2025-03-18 00:49:34 -0300

    create navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,66 @@
+import { useState, useContext, useEffect } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import { useNavigate, useLocation } from 'react-router';
+import { BiFile } from "react-icons/bi";
+import { BiUserCircle } from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
+import './Navbar.css'
+
+export default function Navbar(){
+    const authContext = useContext(AuthContext);
+    const currentUrl = useLocation().pathname;
+    const navigate = useNavigate();
+
+
+    function handleOptionClick(option: 'process' | 'conta'){
+        if(option === 'process'){ navigate('/cliente/lista-processos'); }
+        else{ navigate('/minha-conta'); }
+    }
+
+
+    return(
+        <div className="navbar">
+            <div className="navbar__header-container">
+                <img src="/logo-alt-transp-small.png" alt="Logo PGE-SE" />
+                <div>
+                    <h2>PGE - SE</h2>
+                    <small>
+                        Procuradoria Geral <br />
+                        do Estado de Sergipe
+                    </small>
+                </div>
+            </div>
+
+            <div className="navbar__options-container">
+                <div className="navbar__spacer"></div>
+                <div className={`navbar__option ${(currentUrl.includes('lista-processos'))?'active':''}`} onClick={()=>{handleOptionClick('process')}}>
+                    <BiFile />
+                    <p>Processos</p>
+                </div>
+                <div className="navbar__spacer"></div>
+                <div className={`navbar__option ${(currentUrl.includes('minha-conta'))?'active':''}`} onClick={()=>{handleOptionClick('conta')}}>
+                    <BiUserCircle />
+                    <p>Minha Conta</p>
+                </div>
+                <div className="navbar__spacer"></div>
+            </div>
+
+            
+            <div className='navbar__foorter-container'>
+                <div className="navbar__spacer"></div>
+                <div className='navbar__foorter'>
+                    <BiUser />
+                    <div>
+                        <p>{authContext?.user?.shortName}</p>
+                        <small>
+                            {
+                                (authContext?.user?.role === 'client') ? 'Cliente' : 
+                                (authContext?.user?.role === 'attorney') ? 'Procurador' : ''
+                            }
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
